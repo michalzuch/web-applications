1a6314ed19a5b36feb5f0de05aed1d65ba36476b
michał <51276321+michalzuch@users.noreply.github.com>
2024-03-21 16:28:47 +0100

    [RT] Add Pug Templates
    - create Pug templates for rendering HTML views (index, item)
    - create Pug components for the views (confirmation, footer, form, head-metadata,
      header, item-details, items-grid)
    - set up the template engine in Express to render views using Pug

--- Reservation Tool/app/app.js
+++ Reservation Tool/app/app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = 3000
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
+})
+
+app.get('/item', (req, res) => {
+  res.render('item')
 })
 
 app.listen(port, () => {
